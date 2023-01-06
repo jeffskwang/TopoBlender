d73c81564806befa3ea24c1be4e610be13d9d77f
Jeffrey Kwang <jeffskwang@gmail.com>
2023-01-06 10:57:21 -0600

    examples

--- _render_script_example.sh
+++ _render_script_example.sh
@@ -10,11 +10,11 @@ parameter3=trout_creek_hillshade_example
 blender_location=/Applications/Blender.app/contents/MacOS
 
 ####AUTOMATED SECTION
-#moves to blender location to run blender
-cd $blender_location
-
 #TopoBlender file is assumed to be in the same directory as this .sh file
 TopoBlender_location=$(dirname $0)
+
+#moves to blender location to run blender
+cd $blender_location
 
 ./Blender -b -P $TopoBlender_location/TopoBlender.py -- $TopoBlender_location/parameters/$parameter1
 ./Blender -b -P $TopoBlender_location/TopoBlender.py -- $TopoBlender_location/parameters/$parameter2
